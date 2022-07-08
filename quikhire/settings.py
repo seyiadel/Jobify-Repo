8aa496e6090f5a3d5cb6ae2b996cccba499354f9
seyiadel <seyiadel03@gmail.com>
2022-07-08 15:27:44 +0100

    Added URL to Host

--- quikhire/settings.py
+++ quikhire/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG')
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['jobifyelite22.herokuapp.com', '127.0.0.1']
 
 
 # Application definition
